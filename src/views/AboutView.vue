085b77de711263aab5abeda97169613259ae5366
chenst <chenst@yuanian.com>
2023-10-17 15:43:26 +0800

    feat:自动化

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const { path, params, query } = useRoute()
+
+const seeRote = () => {
+  console.log(path, params, query, '===----')
+}
+</script>
+
+<template>
+  <div class="about">
+    <h1 @click="seeRote">This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
